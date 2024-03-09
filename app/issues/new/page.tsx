43563334339479aa8dfe742f70b57f6a3c7816d9
JieLuis <104972485+JieLuis@users.noreply.github.com>
2024-03-09 19:36:03 +0800

    Handle errors and extract error message

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,65 @@
 "use client";
-import { Button, TextField, TextFieldInput } from "@radix-ui/themes";
+import { Button, TextField, TextFieldInput, Callout } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
+
   return (
-    <div className="p-5 space-y-3 max-w-xl">
-      <TextField.Root>
-        <TextFieldInput placeholder="Title" />
-      </TextField.Root>
-      <SimpleMDE placeholder="description" />
-      <Button>Submit Issue</Button>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+
+      <form
+        className="p-5 space-y-3 "
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+            console.log(data);
+          } catch (error) {
+            setError("An Unexpected Error occurred");
+          }
+          console.log(error);
+        })}
+      >
+        <TextField.Root>
+          <TextFieldInput placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="description" {...field} />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button>Submit Issue</Button>
+      </form>
     </div>
   );
 };
