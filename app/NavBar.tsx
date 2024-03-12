47ad5a8f3a0dcad2415fc64f16725af795c3fec7
JieLuis <104972485+JieLuis@users.noreply.github.com>
2024-03-12 11:09:48 +0800

    Add a loading skeleton for log in

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Skeleton from "@/app/components/Skeleton";
 import React from "react";
 import { PiGithubLogoFill } from "react-icons/pi";
 import Link from "next/link";
@@ -50,7 +51,7 @@ const NavLinks = () => {
 
 const AuthStatus = () => {
   const { status, data: session } = useSession();
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="3rem" />;
   if (status === "unauthenticated")
     return (
       <Link className="nav-link" href="/api/auth/signin">
