4457974202a686b651b3ee2839a24d320857b9b4
JieLuis <104972485+JieLuis@users.noreply.github.com>
2024-03-12 11:04:16 +0800

    Refactor the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -9,47 +9,57 @@ import { Box } from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
 
 const NavBar = () => {
-  const { status, data: session } = useSession();
-  const currentPath = usePathname();
-  const links = [
-    { label: "Blogs", href: "/blogs" },
-    { label: "About me", href: "/about-me" },
-    { label: "instagram", href: "https://www.instagram.com/henrqaz/" },
-  ];
-
   return (
     <nav className="flex space-x-6 broder-b mb-5 px-5 h-14 items-center">
       <Link href="/">
         <PiGithubLogoFill />
       </Link>
       <Link href="/">My Blog</Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classNames({
-                "text-zinc-950": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-              href={link.href}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Log out</Link>
-        )}
-
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
-      </Box>
+      <NavLinks />
+      <AuthStatus />
     </nav>
   );
 };
-
 export default NavBar;
+
+const NavLinks = () => {
+  const links = [
+    { label: "Blogs", href: "/blogs" },
+    { label: "About me", href: "/about-me" },
+    { label: "instagram", href: "https://www.instagram.com/henrqaz/" },
+  ];
+  const currentPath = usePathname();
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              "nav-link": true,
+              "!text-zinc-950": link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Login
+      </Link>
+    );
+  return (
+    <Box>
+      <Link href="/api/auth/signout">Log out</Link>
+    </Box>
+  );
+};
