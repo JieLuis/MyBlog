3ad12a94448f9cfdd178978637b0f3429260d23b
JieLuis <104972485+JieLuis@users.noreply.github.com>
2024-03-13 20:39:46 +0800

    Build blog summary components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import prisma from "@/prisma/client";
+import BlogSummary from "./BlogSummary";
 import LatestBlogs from "./LatestBlogs";
 
-export default function Home() {
-  return <LatestBlogs />;
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  return (
+    <>
+      <LatestBlogs />
+      <BlogSummary open={open} inProgress={inProgress} closed={closed} />
+    </>
+  );
 }
