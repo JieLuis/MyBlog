31d8dda0b862ba5a0aa736ef72ab5127efbd5a2c
JieLuis <104972485+JieLuis@users.noreply.github.com>
2024-03-13 11:22:29 +0800

    Show toast notifications for assignee

--- app/api/blogs/[id]/AssigneeSelect.tsx
+++ app/api/blogs/[id]/AssigneeSelect.tsx
@@ -5,6 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -23,34 +24,37 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   if (isLoading) return <Skeleton />;
 
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || ""}
-      onValueChange={(userId: String | null) => {
-        console.log(userId);
-        userId = userId === "unassigned" ? null : userId;
-        console.log(userId);
-        axios.patch(`/api/blogs/${issue.id}`, {
-          assignedToUserId: userId || null,
-        });
-      }}
-    >
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="unassigned">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item
-              key={user.id}
-              value={user.id}
-              className="cursor-pointer"
-            >
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={async (userId: String | null) => {
+          userId = userId === "unassigned" ? null : userId;
+          await axios
+            .patch(`/api/blogs/${issue.id}`, {
+              assignedToUserId: userId || null,
+            })
+            .catch(() => toast.error("Changes could not be saved"));
+        }}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="unassigned">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item
+                key={user.id}
+                value={user.id}
+                className="cursor-pointer"
+              >
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
